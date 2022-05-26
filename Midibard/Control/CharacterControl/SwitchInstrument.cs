db9048c3df21fc7474a95116a9a8848c2825c66d
Chris B <cjbwork86@gmail.com>
2022-05-26 19:18:31 +0100

    refactored instrument switching

--- Midibard/Control/CharacterControl/SwitchInstrument.cs
+++ Midibard/Control/CharacterControl/SwitchInstrument.cs
@@ -31,31 +31,6 @@ namespace MidiBard.Control.CharacterControl
             });
         }
 
-        public static async Task SwitchToFromHscPlaylist(string fileName)
-        {
-            try
-            {
-                PluginLog.Information($"Instrument switching from hsc playlist for '{fileName}'");
-
-                var songSettings = HSC.Settings.PlaylistSettings.Settings[fileName];
-
-                var firstTrack = songSettings.Tracks.Values.FirstOrDefault(t => !t.Muted && t.EnsembleMember == HSC.Settings.CharIndex);
-                if (firstTrack == null)
-                    return;
-
-                    PluginLog.Information($"switching to '{firstTrack.EnsembleInstrument}' as assigned from hsc playlist");
-
-                    uint insId = (uint)PerformanceHelpers.GetInstrumentFromName(firstTrack.EnsembleInstrument).Value;
-
-                await SwitchTo(insId);
-            }
-
-            catch (Exception e)
-            {
-                PluginLog.Error(e, $"Instrument switching from hsc playlist failed. {e.Message}");
-            }
-        }
-
         public static async Task SwitchTo(uint instrumentId, int timeOut = 3000)
         {
             if (Configuration.config.bmpTrackNames)
@@ -156,7 +131,8 @@ namespace MidiBard.Control.CharacterControl
 
             if (config.useHscOverride && config.switchInstrumentFromHscPlaylist)
             {
-                await SwitchToFromHscPlaylist(songName);
+                uint insId = HSC.PerformHelpers.GetInstrumentFromHscPlaylist(songName);
+                await SwitchTo(insId);
                 return;
             }
 
