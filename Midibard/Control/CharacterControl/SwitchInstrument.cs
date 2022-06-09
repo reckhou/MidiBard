e13ac3e2a8c86949b3a26b77fb488210ee6d2183
SpuriousSnail86 <60520448+SpuriousSnail86@users.noreply.github.com>
2022-06-09 23:15:04 +0100

    Revert "refactored instrument switching"
    This reverts commit db9048c3df21fc7474a95116a9a8848c2825c66d.

--- Midibard/Control/CharacterControl/SwitchInstrument.cs
+++ Midibard/Control/CharacterControl/SwitchInstrument.cs
@@ -31,6 +31,31 @@ namespace MidiBard.Control.CharacterControl
             });
         }
 
+        public static async Task SwitchToFromHscPlaylist(string fileName)
+        {
+            try
+            {
+                PluginLog.Information($"Instrument switching from hsc playlist for '{fileName}'");
+
+                var songSettings = HSC.Settings.PlaylistSettings.Settings[fileName];
+
+                var firstTrack = songSettings.Tracks.Values.FirstOrDefault(t => !t.Muted && t.EnsembleMember == HSC.Settings.CharIndex);
+                if (firstTrack == null)
+                    return;
+
+                    PluginLog.Information($"switching to '{firstTrack.EnsembleInstrument}' as assigned from hsc playlist");
+
+                    uint insId = (uint)PerformanceHelpers.GetInstrumentFromName(firstTrack.EnsembleInstrument).Value;
+
+                await SwitchTo(insId);
+            }
+
+            catch (Exception e)
+            {
+                PluginLog.Error(e, $"Instrument switching from hsc playlist failed. {e.Message}");
+            }
+        }
+
         public static async Task SwitchTo(uint instrumentId, int timeOut = 3000)
         {
             if (Configuration.config.bmpTrackNames)
@@ -131,8 +156,7 @@ namespace MidiBard.Control.CharacterControl
 
             if (config.useHscOverride && config.switchInstrumentFromHscPlaylist)
             {
-                uint insId = HSC.PerformHelpers.GetInstrumentFromHscPlaylist(songName);
-                await SwitchTo(insId);
+                await SwitchToFromHscPlaylist(songName);
                 return;
             }
 
